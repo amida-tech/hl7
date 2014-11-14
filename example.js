ed4bb03399cce53825e0c2c063245dd852fe4b50
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-11-14 10:42:18 -0500

    draft version of module

--- example.js
+++ example.js
@@ -3,19 +3,31 @@
 var fs=require('fs');
 var parser=require('./index.js');
 var serializer=require('./index.js');
+var translate=require('./lib/translate.js');
 
-var data = fs.readFileSync('./test/fixtures/sample2.txt').toString();
+var data = fs.readFileSync('./test/fixtures/sample4.txt').toString().split("\n").join("\r");
 
 var hl7=parser.parseString(data);
 
-console.log(JSON.stringify(hl7,null,4));
+//console.log(JSON.stringify(hl7,null,4));
 
 //repeat-component-subcomponent
 //console.log(hl7[0]["MSH"]["MSH_3"][0][0]);
 //console.log(hl7[2]["NK1"]["NK1_2"][0][0]);
 //console.log(hl7[2]["NK1"]["NK1_3"][0][0]);
 
+
+console.log(hl7[1]);
+
+
 var text=serializer.serializeJSON(hl7);
-console.log(data);
+console.log(data.split("\r").join("\n"));
 console.log();
-console.log(text);
+console.log(text.split("\r").join("\n"));
+console.log();
+console.log(text==data);
+
+//console.log(JSON.stringify(hl7,null,4));
+
+
+console.log(JSON.stringify(translate.translate(hl7),null,4));
--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 
 exports.parseString = require('./lib/parser.js').parseString;
 exports.serializeJSON = require('./lib/serializer.js').serializeJSON;
+exports.translate = require('./lib/translate.js').translate;
 
