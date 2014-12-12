af5d0a196b9431a1a171c6f715683acb630de4a1
Dmitry Kachaev <dmitry.kachaev@gmail.com>
2014-12-11 23:25:31 -0500

    tests for parse/serialize/translate

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ describe('parse.js test', function () {
     var data = "";
 
     before(function () {
-        data = fs.readFileSync('./test/fixtures/isabella_jones_lipid_panel.txt').toString();
+        data = fs.readFileSync('./test/fixtures/isabella_jones_lipid_panel.txt').toString().split("\n").join("\r");
 
     });
 
@@ -17,4 +17,24 @@ describe('parse.js test', function () {
         assert.notStrictEqual(undefined, hl7.parseString(data));
     });
 
+    it('parse', function () {
+        var parsed = hl7.parseString(data);
+
+        assert.equal('JONES', parsed[1][5][0][0]);
+        assert.equal('ISABELLA', parsed[1][5][0][1]);
+    });
+
+    it('translate', function () {
+        var tr = hl7.translate(hl7.parseString(data));
+
+        assert.equal('PID', tr[1]["Segment"]);
+        assert.equal('JONES', tr[1]['Patient Name'][0][0]);
+        assert.equal('ISABELLA', tr[1]['Patient Name'][0][1]);
+    });
+
+    it('serializer', function () {
+        var serialized = hl7.serializeJSON(hl7.parseString(data));
+
+        assert.equal(data, serialized);
+    });
 });
