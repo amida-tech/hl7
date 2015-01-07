efa5b5402b7f8a853a227010d7817849e007caba
Jacob S <jsachs@users.noreply.github.com>
2015-01-06 22:15:05 -0600

    tests fieldrepeat, added testing clarity

--- test/test.js
+++ test/test.js
@@ -13,18 +13,18 @@ describe('parse.js test', function () {
 
     });
 
-    it('should work', function () {
+    it('should parse an HL7 string without error', function () {
         assert.notStrictEqual(undefined, hl7.parseString(data));
     });
 
-    it('parse', function () {
+    it('parses fields with the correct mapping', function () {
         var parsed = hl7.parseString(data);
 
-        assert.equal('JONES', parsed[1][5][0][0]);
+        assert.equal('JONES', parsed[1][5][0][0]); // parsed[SEGMENT][FIELD][FIELDREPEAT][COMPONENT]
         assert.equal('ISABELLA', parsed[1][5][0][1]);
     });
 
-    it('translate', function () {
+    it('translates HL7 sequence number to element name', function () {
         var tr = hl7.translate(hl7.parseString(data));
 
         assert.equal('PID', tr[1]["Segment"]);
@@ -32,9 +32,16 @@ describe('parse.js test', function () {
         assert.equal('ISABELLA', tr[1]['Patient Name'][0][1]);
     });
 
-    it('serializer', function () {
+    it('serializes parsed HL7 to a string', function () {
         var serialized = hl7.serializeJSON(hl7.parseString(data));
 
         assert.equal(data, serialized);
+    });
+
+    it('correctly interprets the FIELDREPEAT character', function () {
+        var parsed = hl7.parseString(data);
+
+        assert.equal('185L29839X64489JLPF', parsed[3][3][0][0]);
+        assert.equal('X64489', parsed[3][3][1][0]);
     });
 });
